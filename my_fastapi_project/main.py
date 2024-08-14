d4d1e163df0a40dd183cb257faed26f5ed79c31d
Prakan <prakansum@gosoft.co.th>
2024-08-14 09:23:35 +0700

    add get names request

--- my_fastapi_project/main.py
+++ my_fastapi_project/main.py
@@ -2,14 +2,38 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+
+@app.get("/names")
+def show_names():
+    return {
+        "status": 200,
+        "message": "success",
+        "response": [
+            "Ben10",
+            "Naruto",
+            "One นะ",
+            "Love Love",
+            "Na ja"
+        ]
+    }
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+
+# run dev
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+# run product
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
